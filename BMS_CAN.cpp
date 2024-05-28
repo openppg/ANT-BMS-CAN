d8e9d6fd430299d0912c116d88097ef4c654fd42
Zach Whitehead <zjwhitehead@users.noreply.github.com>
2024-05-28 16:38:51 -0400

    formatting improvements

--- BMS_CAN.cpp
+++ BMS_CAN.cpp
@@ -1,287 +1,288 @@
 #include "BMS_CAN.h"
 
-BMS_CAN::BMS_CAN(uint8_t csPin, long baudrate) : mcp(csPin), baudrate(baudrate) {
-    soc = 0.0;
-    batteryVoltage = 0.0;
-    batteryCurrent = 0.0;
-    highestCellVoltage = 0.0;
-    lowestCellVoltage = 0.0;
-    batteryCharging = false;
-    chargeWireConnected = false;
-    batteryLowSOC = false;
-    batteryReady = false;
-    dischargeMOSStatus = false;
-    chargeMOSStatus = false;
-    highestTemperature = 0.0;
-    lowestTemperature = 0.0;
-    batteryCycle = 0;
-    energyCycle = 0.0;
-    batteryFailureLevel = 0;
+BMS_CAN::BMS_CAN(uint8_t csPin, long baudrate)
+    : mcp(csPin), baudrate(baudrate) {
+  soc = 0.0;
+  batteryVoltage = 0.0;
+  batteryCurrent = 0.0;
+  highestCellVoltage = 0.0;
+  lowestCellVoltage = 0.0;
+  batteryCharging = false;
+  chargeWireConnected = false;
+  batteryLowSOC = false;
+  batteryReady = false;
+  dischargeMOSStatus = false;
+  chargeMOSStatus = false;
+  highestTemperature = 0.0;
+  lowestTemperature = 0.0;
+  batteryCycle = 0;
+  energyCycle = 0.0;
+  batteryFailureLevel = 0;
 #ifdef FREERTOS_H
-    dataMutex = xSemaphoreCreateMutex();
+  dataMutex = xSemaphoreCreateMutex();
 #endif
 }
 
-bool BMS_CAN::begin() {
-    return mcp.begin(baudrate);
-}
+bool BMS_CAN::begin() { return mcp.begin(baudrate); }
 
 void BMS_CAN::update() {
-    int packetSize = mcp.parsePacket();
-    if (packetSize) {
-        uint8_t data[8];
-        uint32_t id = mcp.packetId();
-        for (int i = 0; i < packetSize; i++) {
-            data[i] = mcp.read();
-        }
-        parsePacket(id, data, packetSize);
+  int packetSize = mcp.parsePacket();
+  if (packetSize) {
+    uint8_t data[8];
+    uint32_t id = mcp.packetId();
+    for (int i = 0; i < packetSize; i++) {
+      data[i] = mcp.read();
     }
+    parsePacket(id, data, packetSize);
+  }
 }
 
 void BMS_CAN::parsePacket(uint32_t id, uint8_t *data, uint8_t len) {
-    if (id == 0x18FF28F4) { // BMS Basic Information 1
-        bool newChargeWireConnected = data[0] & 0x01;
-        bool newBatteryCharging = data[0] & 0x02;
-        bool newBatteryLowSOC = data[0] & 0x04;
-        bool newBatteryReady = data[0] & 0x08;
-        bool newDischargeMOSStatus = data[0] & 0x10;
-        bool newChargeMOSStatus = data[0] & 0x20;
-        float newSOC = data[1];
-        float newBatteryVoltage = (data[4] | (data[5] << 8)) * 0.1;
-        float newBatteryCurrent = ((data[2] | (data[3] << 8)) - 5000) * 0.1;
-        uint8_t newBatteryFailureLevel = data[6];
+  if (id == 0x18FF28F4) { // BMS Basic Information 1
+    bool newChargeWireConnected = data[0] & 0x01;
+    bool newBatteryCharging = data[0] & 0x02;
+    bool newBatteryLowSOC = data[0] & 0x04;
+    bool newBatteryReady = data[0] & 0x08;
+    bool newDischargeMOSStatus = data[0] & 0x10;
+    bool newChargeMOSStatus = data[0] & 0x20;
+    float newSOC = data[1];
+    float newBatteryVoltage = (data[4] | (data[5] << 8)) * 0.1;
+    float newBatteryCurrent = ((data[2] | (data[3] << 8)) - 5000) * 0.1;
+    uint8_t newBatteryFailureLevel = data[6];
 
 #ifdef FREERTOS_H
-        xSemaphoreTake(dataMutex, portMAX_DELAY);
+    xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-        chargeWireConnected = newChargeWireConnected;
-        batteryCharging = newBatteryCharging;
-        batteryLowSOC = newBatteryLowSOC;
-        batteryReady = newBatteryReady;
-        dischargeMOSStatus = newDischargeMOSStatus;
-        chargeMOSStatus = newChargeMOSStatus;
-        soc = newSOC;
-        batteryVoltage = newBatteryVoltage;
-        batteryCurrent = newBatteryCurrent;
-        batteryFailureLevel = newBatteryFailureLevel;
+    chargeWireConnected = newChargeWireConnected;
+    batteryCharging = newBatteryCharging;
+    batteryLowSOC = newBatteryLowSOC;
+    batteryReady = newBatteryReady;
+    dischargeMOSStatus = newDischargeMOSStatus;
+    chargeMOSStatus = newChargeMOSStatus;
+    soc = newSOC;
+    batteryVoltage = newBatteryVoltage;
+    batteryCurrent = newBatteryCurrent;
+    batteryFailureLevel = newBatteryFailureLevel;
 #ifdef FREERTOS_H
-        xSemaphoreGive(dataMutex);
+    xSemaphoreGive(dataMutex);
 #endif
-    } else if (id == 0x18FE28F4) { // BMS Basic Information 2
-        float newHighestCellVoltage = (data[0] | (data[1] << 8)) * 0.001;
-        float newLowestCellVoltage = (data[2] | (data[3] << 8)) * 0.001;
-        float newHighestTemperature = data[4] - 40;
-        float newLowestTemperature = data[5] - 40;
+  } else if (id == 0x18FE28F4) { // BMS Basic Information 2
+    float newHighestCellVoltage = (data[0] | (data[1] << 8)) * 0.001;
+    float newLowestCellVoltage = (data[2] | (data[3] << 8)) * 0.001;
+    float newHighestTemperature = data[4] - 40;
+    float newLowestTemperature = data[5] - 40;
 
 #ifdef FREERTOS_H
-        xSemaphoreTake(dataMutex, portMAX_DELAY);
+    xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-        highestCellVoltage = newHighestCellVoltage;
-        lowestCellVoltage = newLowestCellVoltage;
-        highestTemperature = newHighestTemperature;
-        lowestTemperature = newLowestTemperature;
+    highestCellVoltage = newHighestCellVoltage;
+    lowestCellVoltage = newLowestCellVoltage;
+    highestTemperature = newHighestTemperature;
+    lowestTemperature = newLowestTemperature;
 #ifdef FREERTOS_H
-        xSemaphoreGive(dataMutex);
+    xSemaphoreGive(dataMutex);
 #endif
-    } else if (id == 0x18A328F4) { // Battery Cycle Information
-        uint32_t newBatteryCycle = (data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0];
-        uint32_t newEnergyCycle = (data[7] << 24) | (data[6] << 16) | (data[5] << 8) | data[4];
-        float newEnergyCycleWh = newEnergyCycle * 0.1;
+  } else if (id == 0x18A328F4) { // Battery Cycle Information
+    uint32_t newBatteryCycle =
+        (data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0];
+    uint32_t newEnergyCycle =
+        (data[7] << 24) | (data[6] << 16) | (data[5] << 8) | data[4];
+    float newEnergyCycleWh = newEnergyCycle * 0.1;
 
 #ifdef FREERTOS_H
-        xSemaphoreTake(dataMutex, portMAX_DELAY);
+    xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-        batteryCycle = newBatteryCycle;
-        energyCycle = newEnergyCycleWh;
+    batteryCycle = newBatteryCycle;
+    energyCycle = newEnergyCycleWh;
 #ifdef FREERTOS_H
-        xSemaphoreGive(dataMutex);
+    xSemaphoreGive(dataMutex);
 #endif
-    }
+  }
 }
 
 float BMS_CAN::getSOC() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = soc;
+  float value = soc;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getBatteryVoltage() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = batteryVoltage;
+  float value = batteryVoltage;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getBatteryCurrent() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = batteryCurrent;
+  float value = batteryCurrent;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getPower() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = (batteryVoltage * batteryCurrent) / 1000.0;
+  float value = (batteryVoltage * batteryCurrent) / 1000.0;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getHighestCellVoltage() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = highestCellVoltage;
+  float value = highestCellVoltage;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getLowestCellVoltage() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = lowestCellVoltage;
+  float value = lowestCellVoltage;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 bool BMS_CAN::isBatteryCharging() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    bool value = batteryCharging;
+  bool value = batteryCharging;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 bool BMS_CAN::isChargeWireConnected() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    bool value = chargeWireConnected;
+  bool value = chargeWireConnected;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 bool BMS_CAN::isBatteryLowSOC() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    bool value = batteryLowSOC;
+  bool value = batteryLowSOC;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 bool BMS_CAN::isBatteryReady() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    bool value = batteryReady;
+  bool value = batteryReady;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 bool BMS_CAN::isDischargeMOSStatus() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    bool value = dischargeMOSStatus;
+  bool value = dischargeMOSStatus;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 bool BMS_CAN::isChargeMOSStatus() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    bool value = chargeMOSStatus;
+  bool value = chargeMOSStatus;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getHighestTemperature() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = highestTemperature;
+  float value = highestTemperature;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getLowestTemperature() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = lowestTemperature;
+  float value = lowestTemperature;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 uint32_t BMS_CAN::getBatteryCycle() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    uint32_t value = batteryCycle;
+  uint32_t value = batteryCycle;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 float BMS_CAN::getEnergyCycle() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    float value = energyCycle;
+  float value = energyCycle;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
 
 uint8_t BMS_CAN::getBatteryFailureLevel() {
 #ifdef FREERTOS_H
-    xSemaphoreTake(dataMutex, portMAX_DELAY);
+  xSemaphoreTake(dataMutex, portMAX_DELAY);
 #endif
-    uint8_t value = batteryFailureLevel;
+  uint8_t value = batteryFailureLevel;
 #ifdef FREERTOS_H
-    xSemaphoreGive(dataMutex);
+  xSemaphoreGive(dataMutex);
 #endif
-    return value;
+  return value;
 }
--- BMS_CAN.h
+++ BMS_CAN.h
@@ -1,59 +1,59 @@
 #ifndef BMS_CAN_H
 #define BMS_CAN_H
 
-#include <Arduino.h>
 #include <Adafruit_MCP2515.h>
+#include <Arduino.h>
 
 class BMS_CAN {
 public:
-    BMS_CAN(uint8_t csPin, long baudrate);
-    bool begin();
-    void update();
+  BMS_CAN(uint8_t csPin, long baudrate);
+  bool begin();
+  void update();
 
-    float getSOC();
-    float getBatteryVoltage();
-    float getBatteryCurrent();
-    float getPower();
-    float getHighestCellVoltage();
-    float getLowestCellVoltage();
-    bool isBatteryCharging();
-    bool isChargeWireConnected();
-    bool isBatteryLowSOC();
-    bool isBatteryReady();
-    bool isDischargeMOSStatus();
-    bool isChargeMOSStatus();
-    float getHighestTemperature();
-    float getLowestTemperature();
-    uint32_t getBatteryCycle();
-    float getEnergyCycle();
-    uint8_t getBatteryFailureLevel();
+  float getSOC();
+  float getBatteryVoltage();
+  float getBatteryCurrent();
+  float getPower();
+  float getHighestCellVoltage();
+  float getLowestCellVoltage();
+  bool isBatteryCharging();
+  bool isChargeWireConnected();
+  bool isBatteryLowSOC();
+  bool isBatteryReady();
+  bool isDischargeMOSStatus();
+  bool isChargeMOSStatus();
+  float getHighestTemperature();
+  float getLowestTemperature();
+  uint32_t getBatteryCycle();
+  float getEnergyCycle();
+  uint8_t getBatteryFailureLevel();
 
 private:
-    Adafruit_MCP2515 mcp;
-    long baudrate;
+  Adafruit_MCP2515 mcp;
+  long baudrate;
 
-    float soc;
-    float batteryVoltage;
-    float batteryCurrent;
-    float highestCellVoltage;
-    float lowestCellVoltage;
-    bool batteryCharging;
-    bool chargeWireConnected;
-    bool batteryLowSOC;
-    bool batteryReady;
-    bool dischargeMOSStatus;
-    bool chargeMOSStatus;
-    float highestTemperature;
-    float lowestTemperature;
-    uint32_t batteryCycle;
-    float energyCycle;
-    uint8_t batteryFailureLevel;
+  float soc;
+  float batteryVoltage;
+  float batteryCurrent;
+  float highestCellVoltage;
+  float lowestCellVoltage;
+  bool batteryCharging;
+  bool chargeWireConnected;
+  bool batteryLowSOC;
+  bool batteryReady;
+  bool dischargeMOSStatus;
+  bool chargeMOSStatus;
+  float highestTemperature;
+  float lowestTemperature;
+  uint32_t batteryCycle;
+  float energyCycle;
+  uint8_t batteryFailureLevel;
 
-    void parsePacket(uint32_t id, uint8_t *data, uint8_t len);
+  void parsePacket(uint32_t id, uint8_t *data, uint8_t len);
 
-    // Mutex for thread safety
+  // Mutex for thread safety
 #ifdef FREERTOS_H
-    SemaphoreHandle_t dataMutex;
+  SemaphoreHandle_t dataMutex;
 #endif
 };
 
